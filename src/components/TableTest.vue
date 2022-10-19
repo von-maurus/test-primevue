92b8444f4121a69c34d672bd660fcfec9b033447
macarena@toteat.com <macarena@toteat.com>
2022-10-19 16:58:16 -0300

    primevue test

---
+++ src/components/TableTest.vue
@@ -0,0 +1,49 @@
+<template>
+  <DataTable :value="productos" responsiveLayout="scroll">
+    <Column
+      v-for="col of columns"
+      :field="col.field"
+      :header="col.header"
+      :key="col.field"
+    ></Column>
+  </DataTable>
+</template>
+
+<script>
+import axios from "axios";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
+export default {
+  components: {
+    DataTable,
+    Column,
+  },
+  data() {
+    return {
+      productos: [],
+      columns: [
+        {
+          field: "title",
+          header: "Title",
+        },
+        {
+          field: "description",
+          header: "Description",
+        },
+        {
+          field: "price",
+          header: "Price",
+          numeric: true,
+        },
+      ],
+    };
+  },
+  mounted() {
+    axios.get("https://dummyjson.com/products").then((resp) => {
+      console.log("HOLA", resp);
+      this.productos = resp.data.products;
+    });
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/saga-blue/theme.css"
+
+Vue.use(PrimeVue);
 
 Vue.config.productionTip = false
 
