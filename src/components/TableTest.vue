40ebf57ae701f4fd1c98bd6ed771cc8e58a92b49
macarena@toteat.com <macarena@toteat.com>
2022-10-19 16:39:43 -0300

    buefy test

---
+++ src/components/TableTest.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-table :data="productos" :columns="columns"></b-table>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      productos: [],
+      columns: [
+        {
+          field: "title",
+          label: "Title",
+        },
+        {
+          field: "description",
+          label: "Description",
+        },
+        {
+          field: "price",
+          label: "Price",
+          numeric: true,
+        },
+      ],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://dummyjson.com/products")
+      .then(resp => {
+        console.log("HOLA", resp);
+        this.productos = resp.data.products});
+  },
+};
+</script>
