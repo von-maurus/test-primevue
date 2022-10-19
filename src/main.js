8fc1c156a25fc85f656e00be3d1f034af36dc30e
macarena@toteat.com <macarena@toteat.com>
2022-10-19 17:22:21 -0300

    carpeta dist buefy x componente

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Buefy from 'buefy'
+// import Buefy from 'buefy'
+import { Table } from 'buefy'
 import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
 
-Vue.use(Buefy) 
+// Vue.use(Buefy) 
+Vue.use(Table) 
 
 new Vue({
   render: h => h(App),
